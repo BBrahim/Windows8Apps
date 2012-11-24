2d55feac3e7b023c7c4392b36795df76069d16bb
Gergo Bogdan <bogdangergo@gmail.com>
2012-11-24 20:04:25 +0100

    Added JSON Parsing sample project

---
+++ JsonParsing/JsonParsing/js/Animal.js
@@ -0,0 +1,87 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" /> 
+/// <reference path="//Microsoft.WinJS.1.0/js/ui.js" /> 
+
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Zoo", {
+        Animal: WinJS.Class.define(
+
+            //constructor
+            function () {
+                this._name = "";
+                this._age = "";
+                this._isHungry = false;
+                this._hoursSinceLastFeed = 0;
+            },
+
+            //methods
+            {
+                getName: function () { return this._name; },
+                setName: function (newValue) { this._name = newValue; },
+
+                getAge: function () { return this._age; },
+                setAge: function (newValue) { this._age = newValue; },
+
+                isHungry: function () { return this._isHungry; },
+
+                getHoursSinceLastFeed: function () { return this._hoursSinceLastFeed; },
+                setHoursSinceLastFeed: function (newValue) {
+                    this._hoursSinceLastFeed = newValue;
+
+                    if (newValue > 4) {
+                        this._isHungry = true;
+                    }
+                    else {
+                        this._isHungry = false;
+                    }
+                },
+            },
+
+            //static methods
+            {
+                buildAnimal: function (model) {
+
+                    var newAnimal = new Zoo.Animal();
+                    if (model.hasOwnProperty("name")) {
+                        newAnimal.setName(model.name);
+                    }
+
+                    if (model.hasOwnProperty("age")) {
+                        newAnimal.setAge(model.age);
+                    }
+
+                    if (model.hasOwnProperty("hoursSinceLastFeed")) {
+                        newAnimal.setHoursSinceLastFeed(model.hoursSinceLastFeed);
+                    }
+
+                    return new WinJS.Binding.as(newAnimal);
+                },
+
+                loadZoo: function (uri) {
+                    //IMPORTANT TO RETURN THE PROMISE
+                    return Windows.Storage.StorageFile.getFileFromApplicationUriAsync(uri).then(
+                         function (file) {
+
+                             return Windows.Storage.FileIO.readTextAsync(file).then(
+                                 function (textFromFile) {
+                                     var myParsedJsonData = JSON.parse(textFromFile);
+
+                                     //this will store all the new animals added to zoo
+                                     var zoo = new Array();
+
+                                     if (myParsedJsonData) {
+
+                                         myParsedJsonData.forEach(function (newObject) {
+                                             var newAnimal = Zoo.Animal.buildAnimal(newObject);
+                                             zoo.push(newAnimal);
+                                         });
+                                     }
+                                     return zoo;
+                                 });
+                         });
+                }
+            })//end  WinJS.Class.define
+    });
+})();
---
+++ JsonParsing/JsonParsing/js/default.js
@@ -0,0 +1,60 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+
+            args.setPromise(WinJS.UI.processAll().then(function() {
+               
+                //build up the URL for the file added to the project
+                var url = new Windows.Foundation.Uri("ms-appx:///zoo.json");
+
+                //this will store the imported data
+                var myNewAnimals = new Array();
+
+                //invoke the static method which loads the file
+                //and creates Animal objects from json data
+                //THE METHOD RETURNS A PROMISE!!!
+                Zoo.Animal.loadZoo(url).done(
+
+                    function (result) {
+                        myNewAnimals = result;
+
+                        myNewAnimals.forEach(function (animal) {
+                            console.log("Name: " + animal.getName() + ", Age: " + animal.getAge() + ", IsHungry: " + animal.isHungry() + ", Hours since Last feed: " + animal.getHoursSinceLastFeed());
+                        });
+                    },
+                    function (error) {
+                        var messDialog = new Windows.UI.Popups.MessageDialog(error);
+                        messDialog.showAsync();
+                    });
+
+            }));
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
